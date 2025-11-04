1bf1cdd5ca7c2c3fc2b15aa262ef2c3b89bb7afc
brinvex-dev1 <dev1@brinvex.com>
2025-11-04 21:02:12 +0100

    Upgrade to Java 25

--- release.cmd
+++ release.cmd
@@ -1,6 +1,6 @@
-set JAVA_HOME="C:\tools\java\jdk-23.0.1"
+set JAVA_HOME="v:\tools\java\jdk-25"
 
-set new_version=1.1.0
+set new_version=1.1.1
 
 set jsh_content=^
     Files.writeString(Path.of("README.md"), ^
